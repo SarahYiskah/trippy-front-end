09417d03cbd26a343d9360321455646635733017
github username <github email address>
2018-04-16 10:22:41 -0400

    profile loads with followers and following

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,13 +4,14 @@ import React, { Component}  from 'react';
 class Profile extends Component {
 
   state = {
-    user: {}
+    user: {},
+    followers: [],
+    following: []
   }
 
 
   tryToGetProfile = (propsToLookAt) => {
     if (propsToLookAt.auth) {
-      console.log(propsToLookAt.auth.token)
       fetch(`http://localhost:3000/api/v1/users/${ propsToLookAt.auth.user_id }`, {
         headers:  {
           "Content-Type": "application/json",
@@ -19,23 +20,82 @@ class Profile extends Component {
         }
         })
       .then((res) => res.json())
-      .then((json) => this.setState({
+      .then((json) => {
+      json.error ? this.setState({
+        user: {error: true}
+      }) : this.setState({
         user: json
-      }, () => console.log("you just set the state to", this.state.user)))
+      })})
+    }
+  }
+
+  tryToGetFollowers = (propsToLookAt) => {
+    if (propsToLookAt.auth) {
+      fetch(`http://localhost:3000/api/v1/users/${ propsToLookAt.auth.user_id }/followers`, {
+        headers:  {
+          "Content-Type": "application/json",
+          "Accepts": "application/json",
+          "Authorization": `Token token=${propsToLookAt.auth.token}`
+        }
+        })
+      .then((res) => res.json())
+      .then((json) => {
+      json.error ? this.setState({
+        followers: []
+      }) : this.setState({
+        followers: json
+      })})
+    }
+  }
+
+  tryToGetFollowing = (propsToLookAt) => {
+    if (propsToLookAt.auth) {
+      fetch(`http://localhost:3000/api/v1/users/${ propsToLookAt.auth.user_id }/following`, {
+        headers:  {
+          "Content-Type": "application/json",
+          "Accepts": "application/json",
+          "Authorization": `Token token=${propsToLookAt.auth.token}`
+        }
+        })
+      .then((res) => res.json())
+      .then((json) => {
+      json.error ? this.setState({
+        following: []
+      }) : this.setState({
+        following: json
+      })})
     }
   }
 
   componentDidMount = () => {
     this.tryToGetProfile(this.props)
+    this.tryToGetFollowers(this.props)
+    this.tryToGetFollowing(this.props)
   }
 
   componentWillReceiveProps = (nextProps) => {
     this.tryToGetProfile(nextProps)
+    this.tryToGetFollowers(nextProps)
+    this.tryToGetFollowing(nextProps)
   }
 
   render(){
     return(
-      <div></div>
+      <div>
+      {this.state.user.error ? <h2>You do not have access to this profile</h2> :
+      <div>
+      <h2>{this.state.user.name}</h2>
+      <h3>{this.state.user.email}</h3>
+      {this.state.following.length > 0 ? <div>
+      <h4>Following</h4>
+      {this.state.following.map(friend => <p key={friend.id}>{friend.name}</p>)}
+      </div> : <h4>You are not following anyone</h4>}
+      {this.state.followers.length > 0 ? <div>
+      <h4>Followers</h4>
+      {this.state.followers.map(friend => <p key={friend.id}>{friend.name}</p>)}
+      </div> : <h4>You have no followers</h4>}
+      </div>
+    }</div>
     )
   }
 }
